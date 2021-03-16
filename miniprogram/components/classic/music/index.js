069c103559569beba63b51f5cb36dbd5d0d2b960
Kai <1114366961@qq.com>
2021-03-16 17:36:41 +0800

    完成音乐播放

--- miniprogram/components/classic/music/index.js
+++ miniprogram/components/classic/music/index.js
@@ -21,7 +21,11 @@ Component({
     pauseSrc: 'images/player@pause.png',
     playSrc: 'images/player@play.png',
   },
-  
+  attached(event) {
+    // 跳转页面 当前 切换
+    this._recoverStatus()
+    this._monitorSwitch()
+  },
   detached: function (event) {
     // wx:if hidden
     // mMgr.stop()
@@ -44,6 +48,37 @@ Component({
         })
         mMgr.pause()
       }
+    },
+    _recoverStatus: function () {
+      if (mMgr.paused) {
+        this.setData({
+          playing: false
+        })
+        return
+      }
+      if (mMgr.src == this.properties.src) {
+        this.setData({
+          playing: true
+        })
+      }
+    },
+  
+    _monitorSwitch: function () {
+      mMgr.onPlay(() => {
+        this._recoverStatus()
+      })
+      mMgr.onPause(() => {
+        this._recoverStatus()
+      })
+      mMgr.onStop(() => {
+        this._recoverStatus()
+      })
+      mMgr.onEnded(() => {
+        this._recoverStatus()
+      })
     }
+
   }
+
+
 })
