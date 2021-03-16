febabdc890fe47165bc09db36a42e09c5b4e80be
Kai <1114366961@qq.com>
2021-03-16 22:17:59 +0800

    使用Promise 封裝book.js

---
+++ miniprogram/pages/book/book.js
@@ -0,0 +1,79 @@
+import { BookModel } from "../../models/book"
+
+
+const bookModel = new BookModel()
+// miniprogram/pages/book/book.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const promise = new Promise((resolve,reject)=>{
+        wx.getSystemInfo({
+          
+        })
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    const hotList = bookModel.getHotList()
+    hotList.then(
+      res=>console.log(res)
+    )
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
