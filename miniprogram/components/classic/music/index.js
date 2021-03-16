e30aa08fd4ce9751c480c3aba1f5eb31a1fb7b01
Kai <1114366961@qq.com>
2021-03-16 17:11:11 +0800

    使用wx.getBackgroundAudioManager()播放音乐

--- miniprogram/components/classic/music/index.js
+++ miniprogram/components/classic/music/index.js
@@ -1,5 +1,7 @@
 // components/classic/music/index.js
 import {classicBeh} from '../classic-beh'
+
+const mMgr =wx.getBackgroundAudioManager()
 Component({
   /**
    * 组件的属性列表
@@ -24,6 +26,20 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onPlay: function (event) {
+      // 图片要切换
+      if (!this.data.playing) {
+        this.setData({
+          playing: true
+        })
+        mMgr.src = this.properties.src
+        mMgr.title = this.properties.title
+      } else {
+        this.setData({
+          playing: false
+        })
+        mMgr.pause()
+      }
+    }
   }
 })
