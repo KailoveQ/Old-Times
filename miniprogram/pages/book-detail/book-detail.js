35a817be14e1f685acf263fbe4b4a10ac68d5eff
Kai <1114366961@qq.com>
2021-03-17 15:01:48 +0800

    书籍页面短评实现

--- miniprogram/pages/book-detail/book-detail.js
+++ miniprogram/pages/book-detail/book-detail.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/book-detail/book-detail.js
+// pages/book-detail/book-detail.js
 import {
   BookModel
 } from '../../models/book.js'
@@ -7,11 +7,6 @@ import {
 } from '../../models/like.js'
 const bookModel = new BookModel()
 const likeModel = new LikeModel()
-
-// import {
-//   random
-// } from '../../util/common.js'
-
 
 Page({
 
@@ -29,65 +24,76 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+
+
   onLoad: function (options) {
+    wx.showLoading()
     const bid = options.bid
     const detail = bookModel.getDetail(bid)
     const comments = bookModel.getComments(bid)
     const likeStatus = bookModel.getLikeStatus(bid)
 
-    Promise.all([detail, likeStatus, comments]).then(res => {
-			console.log(res);
-			this.setData({
-				book: res[0],
-				likeCount: res[1].fav_nums,
-				likeStatus: res[1].like_status,
-				comments: res[2].comments
-			})
-			wx.hideLoading();
-		})
 
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    Promise.all([detail, comments, likeStatus])
+      .then(res => {
+        this.setData({
+          book: res[0],
+          comments: res[1].comments,
+          likeStatus: res[2].like_status,
+          likeCount: res[2].fav_nums
+        })
+        wx.hideLoading()
+      })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onLike(event) {
+    const like_or_cancel = event.detail.behavior
+    likeModel.like(like_or_cancel, this.data.book.id, 400)
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  // onFakePost(event) {
+  //   this.setData({
+  //     posting: true
+  //   })
+  // },
 
-  },
+  // onCancel(event) {
+  //   this.setData({
+  //     posting: false
+  //   })
+  // },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  // onPost(event) {
+  //   const comment = event.detail.text || event.detail.value
+  //   if (!comment) {
+  //     return
+  //   }
+  //   if (comment.length > 12) {
+  //     wx.showToast({
+  //       title: '短评最多12个字',
+  //       icon: 'none'
+  //     })
+  //     return
+  //   }
 
-  },
+  //   bookModel.postComment(this.data.book.id, comment)
+  //     .then(res => {
+  //       wx.showToast({
+  //         title: '+ 1',
+  //         icon: "none"
+  //       })
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+  //       this.data.comments.unshift({
+  //         content: comment,
+  //         nums: 1
+  //       })
 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
+  //       this.setData({
+  //         comments: this.data.comments,
+  //         posting: false
+  //       })
+  //     })
+  // },
 
   /**
    * 用户点击右上角分享
