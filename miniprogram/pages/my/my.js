0871a591f0bfcd2205ffc0a1a5f8c83621eb8751
Kai <1114366961@qq.com>
2021-03-18 21:43:25 +0800

    初步实现用户登录

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -24,12 +24,54 @@ Page({
     classics: null,
     loading:Boolean
   },
-  onload(options){
-    wx.getUserInfo()
+  onLoad: function (options) {
+    // wx.getUserInfo({
+    //   success:data=>{
+    //     // success
+    //     console.log(data)
+    //   }
+    // })
+    this.userAuthorized()
   },
-  getUserInfo(event){
-    console.log(event)
-  }
+
+  userAuthorized(){
+    //判断用户是否授权了
+    wx.getSetting({
+      success :data =>{
+        console.log(data)
+        if(data.authSetting['scope.userInfo']){
+          console.log('监测到用户授权过了了')
+          // 然后要让控制用户头像的变量authorized变true
+          wx.getUserInfo({
+            success:data=>{
+              this.setData({
+                authorized: true,
+                userInfo:data.userInfo
+              })
+            }
+          })
+        }else{
+          console.log('用户没有登陆')
+        //  喜欢的书 应该是 0，而且 authorized 得为false
+        }
+      }
+    })
+
+  },
+
+  onGetUserInfo(event){
+    // 用户点击登陆之后执行的函数
+    const userInfo = event.detail.userInfo
+    console.log(userInfo)
+    if(userInfo){
+      this.setData({
+        userInfo,
+        authorized: true
+      })
+    }
+    
+  },
+
   // wx.navigateTo({
     //   url:`/pages/classic-detail/index?cid=${cid}
     //     &type=${type}`
