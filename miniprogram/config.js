7e80f58dcc3a5a7af96822f99939b39665b54f18
Kai <1114366961@qq.com>
2021-03-21 20:18:43 +0800

    修改图层渲染错误

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,6 +1,8 @@
 const config = {
-    api_base_url:'http://bl.talelin.com/v1/',
+    // api_base_url:'http://bl.talelin.com/v1/',
+    api_base_url: 'https://bls.talelin.com/v1/',
     appkey:'AbhC31IG7ruCDp57'
+    // appkey: 'XlPyACBYSGylPXnZ',
   }
   
   export {config};
